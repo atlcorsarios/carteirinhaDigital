7e5ffb084a58ed814855f290da2207717ce53faa
gersonfribeiro <gersonfernandesribeiro48@gmail.com>
2026-01-20 09:48:50 -0300

    fix: adicionado uma marca d'água para o sistema

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
     <Navigation v-if="isLayoutVisible" v-model="drawerOpen" />
     <AppBar v-if="isLayoutVisible" @toggle-drawer="toggleDrawer" />
 
-    <v-main class="main-scroll">
+    <v-main class="main-scroll position-relative">
+      <div class="app-watermark">
+        <img src="/WatermarkAvelito.jpg" />
+      </div>
+
       <Breadcrumbs v-if="isLayoutVisible" />
 
       <v-container fluid class="pt-2">
@@ -18,14 +22,14 @@
 </template>
 
 <script setup lang="ts">
-import { computed, watch, ref } from 'vue';
-import { useRoute } from 'vue-router';
 import Snackbar from '@/components/Snackbar.vue';
 import AppBar from './components/layouts/base/AppBar.vue';
 import Navigation from './components/layouts/base/Navigation.vue';
 import Breadcrumbs from './components/layouts/base/Breadcrumbs.vue';
-import { useI18n } from 'vue-i18n';
 import BtnFabOtherTemplate from './components/layouts/BtnFabOtherTemplate.vue';
+import { useRoute } from 'vue-router';
+import { useI18n } from 'vue-i18n';
+import { computed, watch, ref } from 'vue';
 
 const { t, locale } = useI18n();
 
@@ -42,8 +46,7 @@ watch(
     } else {
       document.title = defaultTitle;
     }
-  },
-  { immediate: true }
+  }, { immediate: true }
 );
 
 const isLayoutVisible = computed(() => {
@@ -67,5 +70,36 @@ v-main {
 .main-scroll {
   height: calc(100vh - var(--v-layout-top));
   overflow-y: auto;
+  background-color: rgb(var(--v-theme-background));
+}
+
+.app-watermark {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 0;
+  pointer-events: none;
+  user-select: none;
+  width: 400px;
+  max-width: 80%;
+}
+
+.app-watermark img {
+  width: 100%;
+  height: auto;
+  opacity: 0.3;
+  mix-blend-mode: multiply;
+  transition: all 0.5s ease;
+}
+
+.v-theme--dark .app-watermark img {
+  opacity: 0.3;
+  filter: invert(1) grayscale(100%);
+  mix-blend-mode: screen;
+}
+
+.z-index-1 {
+  z-index: 1;
 }
 </style>
