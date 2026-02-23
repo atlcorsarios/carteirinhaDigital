118ccc0cf461cf9f3077cbe6d7206c56b831358a
gersonfribeiro <gersonfernandesribeiro48@gmail.com>
2026-02-23 13:49:16 -0300

    fix: adicionando conteúdo no mock para testar moreDetails

--- src/services/resources/products/productsService.ts
+++ src/services/resources/products/productsService.ts
@@ -25,19 +25,35 @@ export const productsServices = {
             recipe: {
               idRecipe: 1,
               description: 'First recipe',
-              preparation: '',
+              preparation: "1. Descasque as cenouras e corte-as em rodelas.\n2. Bata no liquidificador com os ovos e o óleo até obter um creme liso.\n3. Em uma tigela separada, misture o açúcar e a farinha de trigo.\n4. Junte a mistura do liquidificador e mexa bem.\n5. Despeje em uma forma untada e asse em forno pré-aquecido a 180°C por 40 minutos.",
               ingredients: [
                 {
                   idRecipe: 1,
                   amount: 1,
                   ingredient: {
                     idIngredient: 1,
-                    description: '',
+                    description: 'INGREDIENT 1',
                     measurement: 'KG',
                     stock: 4,
                     category: {
                       idCategory: 1,
-                      description: '',
+                      description: 'INGREDIENTS CATEGORY 1',
+                      group: 'INGREDIENTS',
+                      active: true
+                    }
+                  }
+                },
+                {
+                  idRecipe: 1,
+                  amount: 1,
+                  ingredient: {
+                    idIngredient: 1,
+                    description: 'INGREDIENT 1',
+                    measurement: 'KG',
+                    stock: 4,
+                    category: {
+                      idCategory: 1,
+                      description: 'INGREDIENTS CATEGORY 1',
                       group: 'INGREDIENTS',
                       active: true
                     }
@@ -46,7 +62,7 @@ export const productsServices = {
               ],
               category: {
                 idCategory: 1,
-                description: '',
+                description: 'RECIPE CATEGORY 1',
                 group: 'RECIPES',
                 active: true
               },
@@ -64,6 +80,6 @@ export const productsServices = {
   },
 
   async saveProduct() {
-    
+
   }
 }
