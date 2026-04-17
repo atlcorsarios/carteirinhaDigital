35c84a451612c45c6f28510539651b16013b6796
gersonfribeiro <gersonfernandesribeiro48@gmail.com>
2026-04-17 15:19:19 -0300

    fix: componente base para os moreDetails

---
+++ src/components/MoreDetails/BaseMoreDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title class="d-flex sticky-title bg-surface">
+      <div class="text-h6 w-100 me-auto">
+        <slot name="title">{{ title ? t(title) : '' }}</slot>
+      </div>
+
+      <v-icon-btn
+        icon="mdi-close"
+        variant="text"
+        color="info"
+        v-tooltip="t('tooltips.forms.close')"
+        @click="$emit('close')"
+      />
+    </v-card-title>
+    <v-card-text>
+      <slot name="details">DETAILS</slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+defineProps<{
+  title?: string
+}>();
+
+defineEmits(['close']);
+
+</script>
