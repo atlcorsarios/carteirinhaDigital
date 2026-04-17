661c558e35b3beb82cf8bd3f18bca3e73568d8d7
gersonfribeiro <gersonfernandesribeiro48@gmail.com>
2026-04-17 15:45:15 -0300

    fix: cardUsuario ajustado para também receber parceiros

--- src/components/cards/CardUsuario.vue
+++ src/components/cards/CardUsuario.vue
@@ -13,9 +13,41 @@
 <script setup lang="ts">
 import type { IUser } from '@/classes/models/resources/ModelUsuarios';
 import ProfileForm from '../forms/resources/ProfileForm.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const fakeOtp = ref('');
-const usuario = defineModel<IUser>('usuario', { required: true });
+const rawData = defineModel<any>('usuario', { required: true });
+
+const usuario = computed<IUser>({
+  get: () => {
+    const data = rawData.value;
+    console.log("data", data);
+    if (!data) return {} as IUser;
+
+    if (data.usuarios) {
+      return {
+        email: data.usuarios.email || '',
+        username: data.nome_fantasia || data.usuarios.username || '',
+        avatar_url: data.usuarios.avatar_url || '',
+        celular_contato: data.usuarios.celular_contato || '',
+        documento: data.usuarios.documento || '',
+        cargo: data.cargo || 'parceiro',
+        usuario_ativo: data.ativo ?? data.usuarios.usuario_ativo ?? false,
+      } as IUser;
+    }
+
+    return {
+      email: data.email || '',
+      username: data.username || '',
+      avatar_url: data.avatar_url || '',
+      celular_contato: data.celular_contato || '',
+      documento: data.documento || '',
+      cargo: data.cargo || 'usuario',
+      usuario_ativo: data.usuario_ativo ?? false
+    } as IUser;
+  },
+
+  set: () => {}
+});
 
 </script>
