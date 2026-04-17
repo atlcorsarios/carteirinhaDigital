f737829b84a3cc95cbed3e1ce1899b0757d7b7d8
gersonfribeiro <gersonfernandesribeiro48@gmail.com>
2026-04-17 15:43:19 -0300

    fix: refatorando moreOTPDetails para usar o BaseMoreDetails

--- src/components/MoreDetails/MoreOTPDetails.vue
+++ src/components/MoreDetails/MoreOTPDetails.vue
@@ -1,34 +1,19 @@
 <template>
-    <v-card>
-    <v-card-title class="d-flex sticky-title bg-surface">
-      <div class="text-h6 w-100 me-auto">
-        {{ otpCodeDetalhado.codigo }}
-      </div>
-
-      <v-icon-btn
-        icon="mdi-close"
-        variant="text"
-        color="info"
-        v-tooltip="t('tooltips.forms.close')"
-        @click="$emit('close')"
-      />
-    </v-card-title>
-    <v-card-text>
-      <CardOtpDetails
-        :otp-code-detalhado="otpCodeDetalhado"
-      />
-    </v-card-text>
-  </v-card>
+  <BaseMoreDetails
+    :title="otpCodeDetalhado.codigo"
+    @close="$emit('close')"
+  >
+    <template #details>
+      <CardOtpDetails :otp-code-detalhado="otpCodeDetalhado" />
+    </template>
+  </BaseMoreDetails>
 </template>
 
 <script setup lang="ts">
+import BaseMoreDetails from './BaseMoreDetails.vue';
 import CardOtpDetails from '../cards/CardOtpDetails.vue';
 import type { IOTPCodeDetalhado } from '@/classes/models/resources/ModelIOTP';
-import { useI18n } from 'vue-i18n';
-
-const { t } = useI18n();
 
 const props = defineProps<{ otpCodeDetalhado: IOTPCodeDetalhado }>();
 defineEmits(['close']);
-
 </script>
